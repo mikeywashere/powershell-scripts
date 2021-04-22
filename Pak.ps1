a9fbf10d72ea1913b1ad078cd83cf2e8ca987ad0
Michael R. Schmidt <mikenging@hotmail.com>
2021-04-22 14:26:59 -0700

    fixing pak

--- Pak.ps1
+++ Pak.ps1
@@ -1,15 +1,54 @@
 <#
 .SYNOPSIS
-    IP - Install-Package but opinionated
+    Pak - Installing Packages - but very opinionated
 .DESCRIPTION
-    Will first find then install the latest version no matter which provider it is stored in
+    Will first find then install list all found version, allowing the user to choose which to install
 .PARAMETER Name
     A required parameter. Name of the package to install.
+.PARAMETER AllVersions
+    Show all available versions
+.PARAMETER AllowPrerelease
+    Show prerelease versions
+.PARAMETER FullDescription
+    Show the full description. Normally what fits on the remaining line is all that is shown of the desciption.
+.PARAMETER NoUserInput
+    No user input - if you are installing this means the latest version will be installed.
+.PARAMETER IncludeDependencies
+    Include the dependencies in the output
 .LINK
     Install-Package https://docs.microsoft.com/en-us/powershell/module/packagemanagement/install-package?view=powershell-7.1
 .EXAMPLE
-    IP 'Microsoft.Graph'
+    Pak 'Microsoft.Graph'
     Installs Microsoft.Graph
+.EXAMPLE
+    Pak 'Microsoft.Graph'
+    If Microsoft.Graph is not installed.
+    Finds Microsoft.Graph, Gives the user a choice to install the latest version found.
+
+    Example output:
+
+    ----------
+    Finding Microsoft.Graph
+    (1):  Microsoft.Graph | 1.5.0 | PSGallery |Microsoft Graph PowerShell module
+
+    Choose one to install (1) (Ctrl-C to exit):
+    ----------
+
+    For this example you can choose 1 to install or Ctrl-C to exit
+.EXAMPLE
+    Pak 'Microsoft.Graph'
+    If Microsoft.Graph is installed.
+    Finds Microsoft.Graph, Gives the user a choice to install the latest version found.
+
+    Example output:
+
+    ----------
+    Finding Microsoft.Graph
+      (1):  Microsoft.Graph | 1.5.0 | PSGallery |Microsoft Graph PowerShell module
+    * (-):  Microsoft.Graph | 1.0.1 | Installed Version.
+
+    Choose one to install (1) (Ctrl-C to exit):
+    ----------
 .NOTES
     Author: Michael R. Schmidt
     Date:   April 20, 2021
@@ -22,22 +61,19 @@ param (
         HelpMessage = "A required parameter. Name of the package."
     )]
     [string]$Name,
-    [string]$Verb = "Install",
+    [ValidateSet("Install", "Query", IgnoreCase = $true)]
+    [string]$Action = "Install",
     [switch]$AllVersions = $false,
     [switch]$AllowPrerelease = $false,
-    [switch]$FullDescription = $false
+    [switch]$FullDescription = $false,
+    [switch]$NoUserInput = $false,
+    [switch]$IncludeDependencies = $false
 )
 
-function Write-RemainingWidthAtWordBreaks() {
+function Get-NormalizedText() {
     param (
-        [string]$text
+        [string]$Text
     )
-
-    $lines = @()
-
-    $x = $Host.UI.RawUI.CursorPosition.X
-    $w = $Host.UI.RawUI.WindowSize.Width
-    $r = $w - $x
 
     $text = $text.Replace("`r", " ")
     $text = $text.Replace("`n", " ")
@@ -45,22 +81,29 @@ function Write-RemainingWidthAtWordBreaks() {
     while ($text.Contains("  ")) {
         $text = $text.Replace("  ", " ")
     }
+    $text
+}
 
-    $indent = ""
-    for ($i = 0; $i -le $x; $i++) {
-        $indent = "$($indent) "
-    }
+function Format-ToWidthAtWordBreaks() {
+    param (
+        [string]$Text,
+        [string]$FirstLine,
+        [int]$Width
+    )
+
+    $text = Get-NormalizedText $text
+
+    $indent = [system.string]::New(" ", $FirstLine.Length)
 
     $words = $text -split " "
 
-    #$words = "-$($words.count)- " + $words
-
-    $line = ""
+    $lines = @()
+    $line = $FirstLine
 
     $words | foreach-object {
         $current = $_.Trim()
         if (($lines.count -eq 0 -And ($line + $current).Length + 1 -ge $r) -Or
-            ($lines.count -gt 0 -And ($line + $current).Length + 1 -ge $w)) {
+            ($lines.count -gt 0 -And ($line + $current).Length + 1 -ge $screenWidth)) {
             $lines = $lines + $line
             $line = $indent
         }
@@ -78,46 +121,30 @@ function Write-RemainingWidthAtWordBreaks() {
     $alltext
 }
 
-function Write-RemainingWidth() {
+function Format-ToWidth() {
     param (
-        [string]$text,
+        [string]$Text,
+        [int]$Width,
         [switch]$WithElipses = $false
     )
 
-    $x = $Host.UI.RawUI.CursorPosition.X
-    $w = $Host.UI.RawUI.WindowSize.Width
-    $r = $w - $x
+    $text = Get-NormalizedText $text
 
-    $text = $text.Replace("`r", " ")
-    $text = $text.Replace("`n", " ")
-    $text = $text.Replace("  ", " ").Replace("  ", " ").Replace("  ", " ")
-
-    $text = $text.Substring(0, [math]::Min($r - 1, $text.Length))
-    if ($WithElipses -And $text.Length -ge $r - 3) {
-        $text = $text.Substring(0, $r - 3)
+    $text = $text.Substring(0, [math]::Min($Width - 1, $text.Length))
+    if ($WithElipses -And $text.Length -ge $Width - 3) {
+        $text = $text.Substring(0, $Width - 3)
         $text = $text + "..."
     }
     $text
 }
 
-function Write-Console() {
-    param (
-        [string]$text
-    )
+##########
+# Script Code
+##########
 
-    [Console]::Write($text)
-}
+$screenWidth = $Host.UI.RawUI.WindowSize.Width
 
-function Write-ConsoleWithNewLine() {
-    param (
-        [string]$text
-    )
-
-    [Console]::WriteLine($text)
-}
-
-$w = $Host.UI.RawUI.WindowSize.Width
-
+Write-Output "Action $($Action)"
 Write-Output "Finding $($Name)"
 if ($AllowPrerelease) {
     if ($AllVersions) {
@@ -147,20 +174,26 @@ foreach ($package in $packages) {
         $installed = $false
     }
 
+    $line = ""
     if ($installed) {
-        Write-Console "* "
+        $line = "* "
     }
     else {
-        Write-Console "  "
+        $line = "  "
     }
-    Write-Console "($($i)):`t$($package.Name) | $($package.Version) | $($package.Source) |"
+    $line += "($($i)):`t$($package.Name) | $($package.Version) | $($package.Source) | "
+    $w = ($screenWidth - $line.Length)
+    Write-output "WWWWW=$($w) / $($screenWidth) / $($line.Length)"
     if ($FullDescription) {
-        Write-ConsoleWithNewLine "$(Write-RemainingWidthAtWordBreaks $package.Summary)"
+        $lines += Format-ToWidthAtWordBreaks $package.Summary, $line.Length, $screenWidth
+        $lines
+        $lines | Write-Output
     } else {
-        Write-ConsoleWithNewLine "$(Write-RemainingWidth $package.Summary -WithElipses)"
+        $line += Format-ToWidth -Text $package.Summary -Width $w -WithElipses 
+        Write-Output "0000" + $line
     }
     
-    if ($Verb -ieq "Dependencies") {
+    if ($IncludeDependencies) {
         $e = ( $package.Dependencies | foreach-object { $a = $_; $b = $a -split { $_ -eq ":" -or $_ -eq "#" }; $c = $b[1] -split '/'; $d = "$($c[0]) $($c[1])"; $d } )
         $e = $e | Sort-Object
 
@@ -168,7 +201,7 @@ foreach ($package in $packages) {
         $line = $indent
         $e | foreach-object {
             $current = $_.Trim()
-            if (($line + $current).Length + 2 -ge $w) {
+            if (($line + $current).Length + 2 -ge $screenWidth) {
                 Write-Output $line
                 $line = $indent
             }
@@ -192,18 +225,32 @@ if ($installedIndex -eq -1 -And $InstallFound) {
     Write-Output "* (-):`t$($InstallFound.Name) | $($InstallFound.Version) | Installed Version."
 }
 
-if ($Verb -ieq "Dependencies") {
-    exit
-}
+#if ($IncludeDependencies) {
+#    exit
+#}
 
 ## $Installed | ConvertTo-Json
 
 $output = $null
 
-if ($Verb -ieq "Install") {
-    Write-Output ""
-    $Choose = Read-Host -Prompt "Choose one to install (1..$($i)) (Ctrl-C to exit)"
-
+if ($Action -ieq "Install") {
+    $i--
+    if ($NoUserInput) {
+        $Choose = 1
+    }
+    else {
+        Write-Output ""
+        if ($i -eq 1) {
+            $Choose = Read-Host -Prompt "Choose one to install (1) (Ctrl-C to exit)"
+        } 
+        elseif ($i -le 4) {
+            $choices = (1..$i) -Join ","
+            $Choose = Read-Host -Prompt "Choose one to install ($($choices)) (Ctrl-C to exit)"
+        }
+        else {
+            $Choose = Read-Host -Prompt "Choose one to install (1..$($i)) (Ctrl-C to exit)"
+        }
+    }
     $Choose = $Choose - 1
     # $version = [System.Version]::Parse("11.00.9600.17840")
     if ($Choose -ge 0 -And $Choose -le $i) {
@@ -245,5 +292,3 @@ if ($Verb -ieq "Install") {
 # $json = $output | ConvertTo-Json -Depth 5
 # Write-Output "JSON:`r`n$($json)"
 
-Write-Output ""
-Write-Output "Script is finished"
